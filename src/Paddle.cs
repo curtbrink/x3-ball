072382988d263c0a5f1d6622f09a1d1b71739ece
Curt Brink <brinkcu@mail.gvsu.edu>
2023-11-14 23:20:54 -0500

    Make paddle self contained and ready to use

--- src/Paddle.cs
+++ src/Paddle.cs
@@ -4,9 +4,10 @@ using Godot;
 public partial class Paddle : Node2D
 {
   // _size refers to the number of paddle "segments"
-  private int _size = 3;
+  private int _size = 2;
 
-  //
+  const int MaxSize = 3;
+
   private int _paddleTextureWidth;
   private Texture2D _paddleTexture;
 
@@ -19,16 +20,19 @@ public partial class Paddle : Node2D
     var rightCap = GetNode<Sprite2D>("RightCap");
     var rightCapWidth = rightCap.Texture.GetWidth();
 
+    var numberOfNodes = (2 * _size) - 1; // e.g. size 2 = 3 segment nodes, size 3 = 5, etc.
+
     // get total width
-    var totalWidth = leftCapWidth + rightCapWidth + (_size * _paddleTextureWidth);
+    var totalWidth = leftCapWidth + rightCapWidth + (numberOfNodes * _paddleTextureWidth);
     var halfTotal = totalWidth / 2;
 
-    // center is at totalWidth/2
+    // set left and right cap positions
     leftCap.Position = new Vector2((-halfTotal) + (leftCapWidth / 2), 0);
-    rightCap.Position = new Vector2((halfTotal) - (rightCapWidth / 2), 0);
+    rightCap.Position = new Vector2(halfTotal - (rightCapWidth / 2), 0);
 
+    // iterate paddle segments
     var segmentX = (-halfTotal) + leftCapWidth + (_paddleTextureWidth / 2);
-    for (int i = 0; i < _size; i++)
+    for (int i = 0; i < numberOfNodes; i++)
     {
       AddPaddleSegmentAtXCoordinate(segmentX);
       segmentX += _paddleTextureWidth;
@@ -52,14 +56,20 @@ public partial class Paddle : Node2D
     GetTree().CallGroup("paddleSegments", Node.MethodName.QueueFree);
   }
 
-  public void UpdateSize()
+  public void UpdateSize(int numberOfSegments)
   {
-    _size += 2;
-    if (_size == 7)
-    {
-      _size = 1;
-    }
+    _size = Math.Clamp(numberOfSegments, 1, MaxSize);
     UpdateTexturePositions();
+  }
+
+  public void IncreaseSize()
+  {
+    UpdateSize(_size + 1);
+  }
+
+  public void DecreaseSize()
+  {
+    UpdateSize(_size - 1);
   }
 
   public override void _Ready()
@@ -70,7 +80,5 @@ public partial class Paddle : Node2D
     paddleTextureNode.Hide();
 
     UpdateTexturePositions();
-
-    Position = new Vector2(200, 200);
   }
 }
